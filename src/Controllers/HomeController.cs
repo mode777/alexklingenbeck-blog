81b19fe659bc1ab34c880ad9b4bf4393ddac0370
mode777 <mode7@gmx.net>
2017-04-17 11:12:13 +0200

    Changed project structure removed unnecessary deps

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspnet_angular2.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IFileProvider _provider;
+
+        public HomeController(IFileProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
